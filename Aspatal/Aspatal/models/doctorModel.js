09de149fa7f6c0a4e647db234a8596f37ae62cad
D3STRON <30839285+D3STRON@users.noreply.github.com>
2018-01-23 11:03:41 +0530

    awesome updates

---
+++ Aspatal/Aspatal/models/doctorModel.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var bcrypt= require('bcryptjs')
+var Schema = mongoose.Schema;
+var doctorSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+	department: String,
+	patients:[]
+});
+var Doctor = mongoose.model('Doctor',doctorSchema);
+module.exports = Doctor;
+
+module.exports.createNewDoctor =function(newDoctor){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newDoctor.password, salt, function(err, hash) {
+	        newDoctor.password = hash;
+	Doctor(newDoctor).save(function(err){
+		if(err)throw err
+        else{
+			console.log('New Doctor added')
+		}
+	  })
+   });
+});
+}
---
+++ Aspatal/Aspatal/server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/routing')
+
+var app= express()
+var routes = require('./routes/index')
+
+
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport)
+
+
+// Connect Flash
+app.use(flash());
+
+
+app.use(express.static(__dirname + '/angular'));
+app.use('/', routes)
+app.listen(8000,function(){
+	console.log('Listening on 8000');
+})
